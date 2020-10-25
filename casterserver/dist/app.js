cecafe7b50f0b2ed0b58e62e173a22d26700f313
Vitor Roriz <v.r.roriz@gmail.com>
2020-10-25 23:06:11 +0100

    Add header to server reply

--- casterserver/dist/app.js
+++ casterserver/dist/app.js
@@ -9,13 +9,15 @@ var fs = require("fs");
 var app = express();
 var port = 8000;
 var PUBLIC_IMAGES = 'public/images';
+var PUBLIC_VIDEOS = 'public/videos';
 app.use(express.static(path_1.default.join(__dirname, '..', 'public')));
 app.get('/ls', function (req, res) {
     var files = [];
     fs.readdirSync(PUBLIC_IMAGES).forEach(function (file) {
         files.push({ fileName: file });
     });
-    res.send(JSON.stringify(files));
+    res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+    res.end(JSON.stringify(files));
 });
 app.listen(port);
 console.log("Listening on Port " + port);
--- casterserver/src/app.ts
+++ casterserver/src/app.ts
@@ -4,15 +4,17 @@ import fs = require('fs');
 const app = express();
 const port = 8000;
 const PUBLIC_IMAGES = 'public/images';
+const PUBLIC_VIDEOS = 'public/videos';
 type HostedVideo = {fileName: string;}
+
 app.use(express.static(path.join(__dirname,'..', 'public')));
 app.get('/ls', (req, res) => {
     const files: HostedVideo[] = [];
     fs.readdirSync(PUBLIC_IMAGES).forEach(file => {
     files.push({fileName: file});
 });
-
-    res.send(JSON.stringify(files));
+    res.writeHead(200,  {'Access-Control-Allow-Origin': '*'})
+    res.end(JSON.stringify(files));
 });
 
 app.listen(port);
